042f6a66e47deafa31a31b978ab269d94f291be4
mailong395 <maingoclong395@gmail.com>
2022-09-03 23:48:36 +0700

    test

--- src/components/Layout/LeftBar/LeftBar.js
+++ src/components/Layout/LeftBar/LeftBar.js
@@ -4,7 +4,8 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 function LeftBar() {
-    return <h1 className={cx('red-line')}>LeftBar</h1>;
+    var h1 = <h1 className={cx('red-line')}>LeftBar</h1>
+    return h1;
 }
 
 export default LeftBar;
