065c6a10fd92868469a0a182a5dcc4431ab55eec
mailong395 <maingoclong395@gmail.com>
2022-09-07 22:46:05 +0700

    add leftbar

--- client/src/components/Layout/LeftBar/LeftBar.js
+++ client/src/components/Layout/LeftBar/LeftBar.js
@@ -4,8 +4,92 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 function LeftBar() {
-    var h1 = <h1 className={cx('red-line')}>LeftBar</h1>
-    return h1;
+    var h1 = <h1 className={cx('red-line')}>LeftBar</h1>;
+    return (
+        <div className={cx('container-left')}>
+            <img className={cx('avata')} src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"} ></img>
+            <div className={cx('input-search')}>
+                <button className={cx('btn')}>btn</button>
+                <input className={cx('search')} type="text" placeholder="Search" />
+            </div>
+
+            <hr />
+            <div className={cx('list-item')}>
+
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+                <div className={cx('item')}>
+                    <img src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`} alt={"avata"}/>
+                    <div className={cx('content-item')}>
+                        <p>Mai Ngoc Long</p>
+                        <span>Nothing</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default LeftBar;
--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,8 +1,12 @@
+import styles from './Home.module.scss';
+import classNames from 'classnames/bind';
 import { Content, LeftBar, RightBar } from '../../Layout';
+
+const cx = classNames.bind(styles);
 
 function Home() {
     return (
-        <div className="App">
+        <div className={cx("main")}>
             <LeftBar />
             <Content />
             <RightBar />
