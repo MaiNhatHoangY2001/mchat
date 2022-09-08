cc6a8a281b84948b8de4d4b2982f21b94152c0a6
hoang <hoan44pro@gmail.com>
2022-09-09 00:50:19 +0700

    change link login

--- client/src/redux/apiRequest.js
+++ client/src/redux/apiRequest.js
@@ -23,7 +23,7 @@ export const loginUser = async (user, dispatch, navigate, setIsLoading) => {
     dispatch(loginStart());
     try {
         setIsLoading(true);
-        const res = await axios.post('http://localhost:8000/api/login', user, { withCredentials: true });
+        const res = await axios.post('https://real-time-chat-server-123.herokuapp.com/api/login', user, { withCredentials: true });
         dispatch(loginSuccess(res.data));
         setIsLoading(false);
         navigate('/');
