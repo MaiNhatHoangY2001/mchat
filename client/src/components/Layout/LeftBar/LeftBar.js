313285a8a47df28eb95e6db5e2fdcba35cfff7df
mailong395 <maingoclong395@gmail.com>
2022-09-10 00:48:32 +0700

    remove h1 of leftbar

--- client/src/components/Layout/LeftBar/LeftBar.js
+++ client/src/components/Layout/LeftBar/LeftBar.js
@@ -6,7 +6,6 @@ import ComboBox from './AutoComplete';
 const cx = classNames.bind(styles);
 
 function LeftBar() {
-    var h1 = <h1 className={cx('red-line')}>LeftBar</h1>;
     return (
         <div className={cx('container-left')}>
             <img
