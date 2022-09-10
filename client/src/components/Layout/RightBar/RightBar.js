80205dc0e0a772ae6a56acf741c3012c4e8bca11
Madnok <hungviper016@gmail.com>
2022-09-10 21:24:01 +0700

    right bar changes

--- client/src/components/Layout/RightBar/RightBar.js
+++ client/src/components/Layout/RightBar/RightBar.js
@@ -5,8 +5,16 @@ const cx = classNames.bind(styles);
 
 function RightBar() {
     return (
-        <div className={cx('flex-column', 'container-right')}>
-            <h1>gey</h1>
+        <div className={cx('flex-column', 'main-right')}>
+            <div className={cx('title')}>
+                <h2>Thông Tin Cuộc Trò Chuyện</h2>
+            </div>
+
+            <div className={cx('flex-row','button-right')}>
+                <button className={cx('btn')}>File</button>
+                <button className={cx('btn')}>Media</button>
+                <button className={cx('btn')}>Link</button>
+            </div>
         </div>
     );
 }
