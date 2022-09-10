cda91285ceb9d835b417bfe017d99f62553f1523
Madnok <hungviper016@gmail.com>
2022-09-10 20:39:17 +0700

    add right bar

--- client/src/components/Layout/RightBar/RightBar.js
+++ client/src/components/Layout/RightBar/RightBar.js
@@ -4,7 +4,11 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 function RightBar() {
-    return <h1 className={cx('red-line')}>RightBar</h1>;
+    return (
+        <div className={cx('flex-column', 'container-right')}>
+            <h1>gey</h1>
+        </div>
+    );
 }
 
 export default RightBar;
