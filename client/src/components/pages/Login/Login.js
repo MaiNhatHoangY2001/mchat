7ca73491651ea1a60f06ded1199d38fcb2920d4b
Tuan2210 <quangtuan496>
2022-09-11 18:47:26 +0700

    fix all frm login

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import 'w3-css/w3.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styled from 'styled-components';
 
 const cx = classNames.bind(styles);
 
@@ -35,99 +34,46 @@ function Login() {
         }
     })
 
-    const a = styled.a`
-    &:hover {
-        color: #D57AD4;
-        cursor: pointer;
-        text-decoration: underline;
-    }`;
-
     return (
-        // <section className={cx('login-container')}>
-             
-        //     <div className={cx('login-title')}> Log in</div>
-        //     <form onSubmit={handleLogin}>
-        //         <label>USERNAME</label>
-        //         <input
-        //             type="text"
-        //             placeholder="Enter your username"
-        //             onChange={(e) => {
-        //                 setUserName(e.target.value);
-        //             }}
-        //         />
-        //         <label>PASSWORD</label>
-        //         <input
-        //             type="password"
-        //             placeholder="Enter your password"
-        //             onChange={(e) => {
-        //                 setPassword(e.target.value);
-        //             }}
-        //         />
-        //         {isLoading ? <p>currently loading</p> : <button type="submit"> Continue </button>}
-                
-        //     </form>
-        //     <div className={cx('login-register')}> Don't have an account yet? </div>
-        //     <Link className={cx('login-register-link')} to="/register">
-        //         Register one for free{' '}
-        //     </Link>
-        // </section>
-        <div className='table'>
-            <div className='row'>
-                <div className={cx('col col-left')} 
-                     style={{ backgroundImage: "url('https://data-mline-congnghemoi.s3.ap-southeast-1.amazonaws.com/bgcolor.png')",
-                              backgroundSize: "cover",
-                              height: "100vh"}}>
-                    <div className={cx('box-left')}>
-                        <img src={'https://data-mline-congnghemoi.s3.ap-southeast-1.amazonaws.com/logo-no-bg.png'} alt={"logo=MLine"}/>
-                        <span id={cx("line")}><b>LINE</b></span>
+        <body>
+            <section className={cx('login-container')}>
+                <div className='row'>
+                    <div className={cx('logo')}>
+                        <img src={'https://data-mline-congnghemoi.s3.ap-southeast-1.amazonaws.com/logo-no-bg.png'} alt={"logo=MLine"} />
+                        <span id={cx("line")}>LINE</span>
                     </div>
                 </div>
-
-                <div className={cx('col col-right')}
-                     style={{border: "1px solid black"}}>
-                    <div className={cx("row-top")}
-                         style={{marginTop: "10%"}}>
-                        <h1>THÔNG TIN NHẬP</h1>
-                        <form onSubmit={handleLogin}>
-                        <div className={cx("rowTenTK")}>
-                            <span>Tên tài khoản:</span>
-                            <input type="text" placeholder="Nhập tên tài khoản" className={cx("inputUser")} 
+                    <div className={cx('login-title')}>Đăng nhập tài khoản MLine</div>
+                    <form onSubmit={handleLogin}>
+                            <div className='col-lg-10'>
+                                <label>Tài khoản:</label>
+                                <input
+                                    type="text"
+                                    placeholder="Nhập tên tài khoản"
                                     onChange={(e) => {
                                         setUserName(e.target.value);
                                     }}
-                            />
-                        </div>
-                        <div className={cx("rowMK")}>
-                            <span>Mật khẩu:</span>
-                            <input type="password" placeholder="Nhập mật khẩu" className={cx("inputPW")} 
+                                /><br/>
+                                <label>Mật khẩu:</label>
+                                <input
+                                    type="password"
+                                    placeholder="Nhập mật khẩu"
                                     onChange={(e) => {
                                         setPassword(e.target.value);
                                     }}
-                            />
-                        </div>
-                        <div className={cx("rowQuenMK")}>
-                            <a href="../ForgotPass/ForgotPass"><b>Quên mật khẩu?</b></a>
-                        </div>
-                        <div className={cx("rowBtnDN")}>
-                            {isLoading ? <p>currently loading</p> : <button type="submit"><b>ĐĂNG NHẬP</b></button>}
-                        </div>
-                        </form>
-                        <div className={cx("rowChuaCoTK")}><span>Bạn chưa có tài khoản?</span></div>
-                        <div className={cx("rowDKNgay")}>
-                            <a href="../Register/Register" className={cx("labelDKNgay")}><b>Đăng ký ngay</b></a>
-                        </div>
-                    </div>
-                    <div className={cx("row-bottom")}>
-                        <div className={cx("w3-bar")}>
-                            <div className={cx("row")}>
-                                <a href="../Register/Register" className={cx("w3-bar-item","w3-button","btnDK")}><b>ĐĂNG KÝ</b></a>
-                                <a href="" className={cx("w3-bar-item","w3-button","btnDN","active-tab")}><b>ĐĂNG NHẬP</b></a>
+                                />
+                                <Link className={cx('forgotpw-link')} to="/forgotpass">
+                                    Bạn quên mật khẩu?{' '}
+                                </Link>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        {isLoading ? <p><i>Đang đăng nhập...</i></p> : <button type="submit">ĐĂNG NHẬP</button>}
+                    </form>
+                    <div className={cx('login-register')}>Bạn chưa có tài khoản?</div>
+                    <Link className={cx('login-register-link')} to="/register">
+                        Đăng ký ngay{' '}
+                    </Link>
+            </section>
+        </body>
     );
 }
 
