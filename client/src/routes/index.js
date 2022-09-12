5005563a1361c52dfed3fc3d528e5b02d4cc08ab
Madnok <hungviper016@gmail.com>
2022-09-12 23:09:34 +0700

    call button added

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,10 +1,12 @@
-import { Home, Register, Login, ForgotPass } from '../components/pages';
+
+import { Home, Register, Login, ForgotPass,Call } from '../components/pages';
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/Register', component: Register },
     { path: '/login', component: Login },
     { path: '/forgotPass', component: ForgotPass },
+    { path: '/call', component: Call },
     
 ];
 
