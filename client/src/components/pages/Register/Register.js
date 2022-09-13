b9a644e04a723bfb94d48f5da35431b1d67036c5
stoky71 <81395239+stoky71@users.noreply.github.com>
2022-09-13 14:40:11 +0700

    fix gd register

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link,useNavigate } from 'react-router-dom';
 import { registerUser } from '../../../redux/apiRequest/authApiRequest';
-
+//import {Formik, useFormik} from 'formik'
 const cx = classNames.bind(styles);
 
 function Register() {
@@ -18,11 +18,25 @@ function Register() {
     const [date, setDate] = useState('');
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [repass, setRePass] = useState('');
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+//    const reSDT = "^(0[0-9]{9}$)";
+    // const xacNhanPass = (e) =>{
+    //     const pass = password.toString;
+    //     const xnpass = repass.toString;
+    //     if(xnpass == pass) 
+    //         document.getElementById('spanspass').innerText("V");
+    //     else
+    //         document.getElementById('spanspass').innerHTML("")
+    // }
     const handleRegister = (e) => {
         e.preventDefault();
+        // const validationForm = this.validationForm();
+        // if(validationForm.error){
+        //     alert(validationForm.msg);
+        // }
         const newUser = {
             userName: userName,
             password: password,
@@ -34,6 +48,7 @@ function Register() {
         };
         registerUser(newUser, dispatch, navigate, setIsLoading);
     };
+    
 
     useEffect(() => {
         if(user){
@@ -41,16 +56,50 @@ function Register() {
         }
     })
 
+    // const validate = values => {
+    //     const errors = {};
+      
+    //     if (!values.firstName) {
+    //       errors.firstName = 'Required';
+    //     } else if (values.firstName.length > 15) {
+    //       errors.firstName = 'Must be 15 characters or less';
+    //     }
+      
+    //     if (!values.lastName) {
+    //       errors.lastName = 'Required';
+    //     } else if (values.lastName.length > 20) {
+    //       errors.lastName = 'Must be 20 characters or less';
+    //     }
+      
+    //     if (!values.email) {
+    //       errors.email = 'Required';
+    //     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    //       errors.email = 'Invalid email address';
+    //     }
+      
+    //     return errors;
+    //   };
+    //   const formik = useFormik({
+    //     initialValues: {
+    //       firstName: '',
+    //       lastName: '',
+    //       email: '',
+    //     },
+    //     validate,
+    //     onSubmit: values => {
+    //       alert(JSON.stringify(values, null, 2));
+    //     },
+    //   });
     return (
         // <body>
             <section className={cx('register-container')}>
                 <form onSubmit={handleRegister} className={cx('register-form')}>
                     <h2> THÔNG TIN ĐĂNG KÝ </h2>
-                    <div className='col-lg-8'>
+                    <div className='col-lg-6'>
                         
                             <label className={cx('margininput')}>Tài khoản:</label>
                             <input type="text" placeholder="Nhập tên tài khoản" 
-                               
+                               id="userName" name="userName"
                                 onChange={(e) => setUserName(e.target.value)} />                           
                         
 
@@ -64,18 +113,20 @@ function Register() {
                         {/* </div> */}
 
                         {/* <div className={cx('row-register')}> */}
-                            <label className={cx('margininput')}>Xác nhận:</label> 
+                            <label className={cx('margininput')}>&nbsp;Xác nhận:</label> 
                             <input
                                 type="password"
                                 placeholder="Nhập lại mật khẩu"
                                 //className={cx('input-register')}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => setRePass(e.target.value)}
+                    //            onBlur={(e) => xacNhanPass(e.target.value)}
                                 />
+                            <span id="spanspass"></span>
                             <br/>
                         {/* </div> */}
 
                         {/* <div className={cx('row-register')}> */}
-                            <label className={cx('margininput')}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Họ:</label> 
+                            <label className={cx('margininput')}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Họ:</label> 
                              <input 
                                 type="text" 
                                 placeholder="Nhập họ" 
@@ -103,22 +154,22 @@ function Register() {
                         {/* </div> */}
 
                         {/* <div className={cx('row-register')}> */}
-                            <label className={cx('margininput')}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email:</label>
+                            <label className={cx('margininput')}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email:</label>
                             <input 
-                                type="text" 
+                                type="email" 
                                 placeholder="Nhập email" 
                                 // className={cx('input-register')}
                                 onChange={(e) => setEmailID(e.target.value)}/>   
                         {/* </div> */}
                         {/* <div className={cx('row-register')}> */}
-                            <label className={cx('margininput')}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SĐT:</label>
+                            <label className={cx('margininput')}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SĐT:</label>
                             <input 
                                 type="text" 
                                 placeholder="Nhập số điện thoại" 
                                 // className={cx('input-register')}
                                 onChange={(e) => setPhoneNumber(e.target.value)}/>
                         {/* </div>  */}
-
+                        <br/> <br/>
                         {/* <div className={cx('row-register')}> */}
                             <label className={cx('marginbutton')}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bạn đã có tài khoản?</label>      
                             <Link to="/login" className={cx('login-link')}>
@@ -126,7 +177,7 @@ function Register() {
                             </Link>
                         {/* </div> */}
                     </div>     
-                            {isLoading ? <p>Đang tạo tài khoản, vui lòng chờ trong giây lát</p> :  <button type="submit"> ĐĂNG KÝ</button>}
+                            {isLoading ? <p>Đang tạo tài khoản, vui lòng chờ trong giây lát</p> :  <button type="submit" className={cx('btnRegister')}> ĐĂNG KÝ</button>}
                       
                 </form>
             </section>
