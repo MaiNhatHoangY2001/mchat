6e344692a6025f41cd217ac30419fdc6f4ff907c
hoang <hoan44pro@gmail.com>
2022-10-13 21:52:18 +0700

    fix bug message

--- client/src/components/Layout/LeftBar/LeftBar.js
+++ client/src/components/Layout/LeftBar/LeftBar.js
@@ -67,7 +67,7 @@ function LeftBar() {
         if (currentIndividualChat !== null) {
             const listChat = currentIndividualChat.concat(currentGroupChat);
             const listSort = listChat.sort(function (a, b) {
-                return new Date(b.message[0]?.time) - new Date(a.message[0]?.time);
+                return new Date(b?.message[0]?.time) - new Date(a?.message[0]?.time);
             });
             setChatActors(listSort);
         }
