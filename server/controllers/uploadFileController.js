6e7a959a3c9b794f2e8479216298ff15ec221381
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2022-10-22 21:18:29 +0700

    change google-cloud-key

--- server/controllers/uploadFileController.js
+++ server/controllers/uploadFileController.js
@@ -3,7 +3,7 @@ const { format } = require('util');
 const { Storage } = require('@google-cloud/storage');
 // Instantiate a storage client with credentials
 const storage = new Storage({ keyFilename: 'google-cloud-key.json' });
-const bucket = storage.bucket('mchat-bucket-storage');
+const bucket = storage.bucket('cloud-storage-mchat');
 
 const upload = async (req, res) => {
 	try {
