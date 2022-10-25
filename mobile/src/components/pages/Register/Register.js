33b13828da0c9f92de824bbafc6b582592b27cc1
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2022-10-25 11:43:42 +0700

    fix

---
+++ mobile/src/components/pages/Register/Register.js
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native';
+import styles from './Register.module.scss';
+import { Link } from 'react-router-native';
+function Register() {
+    return (
+        <View style={styles.registercontainer}>
+            <View>
+                <Text>Thông tin đăng ký</Text>
+                <Link to="/">
+                    <Text> Home </Text>
+                </Link>
+            </View>
+        </View>
+    );
+}
+
+export default Register;
