82b4243f874538a92be58504338bc5f713b16629
stoky71 <81395239+stoky71@users.noreply.github.com>
2022-10-25 21:48:26 +0700

    update screen dangky moblie

--- mobile/src/components/pages/Register/Register.js
+++ mobile/src/components/pages/Register/Register.js
@@ -7,7 +7,6 @@ import { Link } from 'react-router-native';
 
 function Register() {
     return (
-<<<<<<< HEAD
         <SafeAreaView style={{margin:0, padding:0}}>
             <View style={styles.registercontainer}>
                 <ImageBackground source={require('../../../../assets/bgcolor-vertical.png')} style={styles.ImageBackground}>
@@ -20,29 +19,16 @@ function Register() {
                             <Text style={styles.line}>LINE</Text>
                         </View>
                         <Text style={styles.tittle}>Nhập số điện thoại của bạn</Text>
-                        <Text style={styles.info}> Vui lòng nhập số điện thoại đăng ký của bạn</Text>
+                        <Text style={styles.info}> Vui lòng nhập số điện thoại đăng ký của bạn..</Text>
                         {/* <Text><Icon name='globe'/>Vietnamse</Text> */}
                         <TextInput placeholder='      Số điện thoại' style={styles.inputSDT}>
                             {/* <Image source={require('../../../../assets/iconglobe.png')} style={{height:26, width:26}}/> */}
                         </TextInput>
-                        <View style={styles.viewbot}>
-                            <Link to="/" style={styles.btnCon}>
-                                <Text style={styles.txtCon}> Tiếp tục </Text>
-                            </Link>
-                            <Link to="/" style={styles.btnCon}>
-                                <Text style={styles.txtCon}>   Trở về </Text>
-                            </Link>
-                        </View>
+                        <Link to="/" style={styles.btnCon}>
+                            <Text style={styles.txtCon}> Trở v </Text>
+                        </Link>
                     </View>
                 </ImageBackground>
-=======
-        <View style={styles.registercontainer}>
-            <View>
-                <Text>Thông tin đăng ký</Text>
-                {/* <Link to="/">
-                    <Text> Home </Text>
-                </Link> */}
->>>>>>> 506785f2de0e2682012a520fd3121770516e4829
             </View>
         </SafeAreaView>
     );
