29eacf70dd37194c72b45f7af35ad3243ceb9b2a
mailong395 <maingoclong395@gmail.com>
2022-10-27 01:16:25 +0700

    add group imgae in chat

--- client/src/components/Layout/Content/Display/Chat.js
+++ client/src/components/Layout/Content/Display/Chat.js
@@ -21,6 +21,7 @@ import moment from 'moment';
 import Data from './DataHeaderButtonChat';
 import ReactTooltip from 'react-tooltip';
 import Picker from 'emoji-picker-react';
+import { ImageList, ImageListItem } from '@mui/material';
 
 const cx = classNames.bind(styles);
 
@@ -51,6 +52,57 @@ function Chat({ setRightBar }) {
             },
         },
     ]);
+
+    const itemData = [
+        {
+            img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+            title: 'Breakfast',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+            title: 'Burger',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+            title: 'Camera',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+            title: 'Coffee',
+        },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        //     title: 'Hats',
+        // },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        //     title: 'Honey',
+        // },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+        //     title: 'Basketball',
+        // },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+        //     title: 'Fern',
+        // },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+        //     title: 'Mushrooms',
+        // },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+        //     title: 'Tomato basil',
+        // },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+        //     title: 'Sea star',
+        // },
+        // {
+        //     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+        //     title: 'Bike',
+        // },
+    ];
 
     const dispatch = useDispatch();
 
@@ -354,6 +406,25 @@ function Chat({ setRightBar }) {
                             );
                         })
                     )}
+                    <div className={cx('friendSend')}>
+                        <img
+                            className={cx('imgChat')}
+                            src={`https://demoaccesss3week2.s3.ap-southeast-1.amazonaws.com/avata01.png`}
+                            alt="avata"
+                        />
+                        <div data-tip={'test'} data-for="registerTip" className={cx('boxTextChat')}>
+                            <div className={cx('groupImage')}>
+                                {itemData.map((item) => (
+                                    <img src={`${item.img}?w=164&h=164&fit=crop&auto=format`} alt={item.title} />
+                                ))}
+                            </div>
+                        </div>
+                        <div className={cx('boxEdite')}>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
 
                     <div ref={bottomRef} />
                 </div>
