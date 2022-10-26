de74ebcc1525f035242d4c741e320e21ec5e3ece
Tuan2210 <quangtuan496>
2022-10-26 09:15:21 +0700

    add animation screen login mobile

--- mobile/src/components/pages/Login/Login.js
+++ mobile/src/components/pages/Login/Login.js
@@ -17,156 +17,105 @@ function Login() {
     // const [animationName, setAnimationName] = useState('');
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{ display: !flag ? 'flex' : 'none' }}>
-                <ImageBackground source={require('../../../../assets/bgcolor-vertical.png')} style={styles.imgBG}>
-                    <View>
-                        <Image
-                            source={require('../../../../assets/logo-no-bg.png')}
-                            style={{ width: 180, height: 150, resizeMode: 'contain' }}
-                        />
-                        <Text style={styles.txtLine}>LINE</Text>
-                    </View>
-                    <View>
-                        <Image
-                            source={require('../../../../assets/Illustration.png')}
-                            style={{ height: 270, resizeMode: 'contain' }}
-                        />
-                    </View>
-                    <View>
-                        <TouchableOpacity
+            <Animatable.View
+                animation="zoomIn"
+                // iterationCount={5}
+                direction="alternate"
+            >
+                <View style={{ display: !flag ? 'flex' : 'none' }}>
+                    <ImageBackground source={require('../../../../assets/bgcolor-vertical.png')} style={styles.imgBG}>
+                        <View>
+                            <Image
+                                source={require('../../../../assets/logo-no-bg.png')}
+                                style={{ width: 180, height: 150, resizeMode: 'contain' }}
+                            />
+                            <Text style={styles.txtLine}>LINE</Text>
+                        </View>
+                        <View>
+                            <Image
+                                source={require('../../../../assets/Illustration.png')}
+                                style={{ height: 270, resizeMode: 'contain' }}
+                            />
+                        </View>
+                        <View>
+                            <TouchableOpacity
+                                style={{
+                                    shadowColor: 'rgba(0,0,0, .4)', // IOS
+                                    shadowOffset: { height: 1, width: 1 }, // IOS
+                                    shadowOpacity: 2, // IOS
+                                    shadowRadius: 1, //IOS
+                                    backgroundColor: '#fff',
+                                    elevation: 4, // Android
+                                    backgroundColor: '#fff',
+                                    borderRadius: 12,
+                                    borderColor: 'rgb(250, 139, 158)',
+                                    borderWidth: 2,
+                                    borderStyle: 'solid',
+                                    padding: '3%',
+                                }}
+                                onPress={() => setFlag(true)}
+                            >
+                                <Text style={{ color: 'rgb(250, 139, 158)', fontSize: 17, fontWeight: 'bold' }}>
+                                    Đăng nhập bằng số điện thoại
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </ImageBackground>
+                </View>
+                <View style={{ display: !flag ? 'none' : 'flex' }}>
+                    <ImageBackground source={require('../../../../assets/bgcolor-vertical.png')} style={styles.imgBG}>
+                        <View
                             style={{
+                                backgroundColor: '#fff',
+                                width: 350,
+                                height: 450,
+                                padding: 20,
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-around',
                                 shadowColor: 'rgba(0,0,0, .4)', // IOS
                                 shadowOffset: { height: 1, width: 1 }, // IOS
                                 shadowOpacity: 2, // IOS
-                                shadowRadius: 1, //IOS
-                                backgroundColor: '#fff',
-                                elevation: 4, // Android
-                                backgroundColor: '#fff',
-                                borderRadius: 12,
-                                borderColor: 'rgb(250, 139, 158)',
-                                borderWidth: 2,
-                                borderStyle: 'solid',
-                                padding: '3%',
+                                shadowRadius: 2, //IOS
+                                elevation: 10, // Android
+                                borderRadius: 20,
                             }}
-                            onPress={() => setFlag(true)}
                         >
-                            <Text style={{ color: 'rgb(250, 139, 158)', fontSize: 17, fontWeight: 'bold' }}>
-                                Đăng nhập bằng số điện thoại
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </ImageBackground>
-            </View>
-            <View style={{ display: !flag ? 'none' : 'flex' }}>
-                <ImageBackground source={require('../../../../assets/bgcolor-vertical.png')} style={styles.imgBG}>
-                    <View
-                        style={{
-                            backgroundColor: '#fff',
-                            width: 350,
-                            height: 450,
-                            padding: 20,
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-around',
-                            shadowColor: 'rgba(0,0,0, .4)', // IOS
-                            shadowOffset: { height: 1, width: 1 }, // IOS
-                            shadowOpacity: 2, // IOS
-                            shadowRadius: 2, //IOS
-                            elevation: 10, // Android
-                            borderRadius: 20,
-                        }}
-                    >
-                        <View style={{ flexDirection: 'row' }}>
-                            <TextInput
-                                style={styles.styleInput}
-                                placeholder="Số điện thoại"
-                                maxLength={10}
-                                keyboardType="numeric"
-                                // onChangeText={(infoToDo) => setInfoToDo(infoToDo)}
-                                // value={infoToDo}
-                            />
-                            <View style={{ justifyContent: 'center', marginLeft: 10 }}>
-                                <Icon name="phone-portrait-outline" size={40} color="rgb(250, 139, 158)" />
+                            <View style={{ flexDirection: 'row' }}>
+                                <TextInput
+                                    style={styles.styleInput}
+                                    placeholder="Số điện thoại"
+                                    maxLength={10}
+                                    keyboardType="numeric"
+                                    // onChangeText={(infoToDo) => setInfoToDo(infoToDo)}
+                                    // value={infoToDo}
+                                />
+                                <View style={{ justifyContent: 'center', marginLeft: 10 }}>
+                                    <Icon name="phone-portrait-outline" size={40} color="rgb(250, 139, 158)" />
+                                </View>
                             </View>
-                        </View>
-                        <Text style={styles.errorMess}>Lỗi sdt</Text>
-                        <View style={{ flexDirection: 'row' }}>
-                            <TextInput style={styles.styleInput} placeholder="Mật khẩu" />
-                            <TouchableOpacity
-                                style={{
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    alignSelf: 'center',
-                                    borderColor: 'cyan',
-                                    borderWidth: 1,
-                                    borderStyle: 'solid',
-                                    height: 40,
-                                    paddingLeft: 5,
-                                    paddingRight: 5,
-                                    marginLeft: 10,
-                                }}
-                            >
-                                <Icon name="eye-sharp" size={30} color="rgb(250, 139, 158)" />
-                            </TouchableOpacity>
-                        </View>
-                        <Text style={styles.errorMess}>Lỗi mk</Text>
-                        <Link to="/forgotPass" style={{ alignSelf: 'flex-end', marginRight: 22 }}>
-                            <Text
-                                style={{
-                                    color: 'rgb(250, 139, 158)',
-                                    fontWeight: 'bold',
-                                    fontSize: 15,
-                                    textDecorationLine: 'underline',
-                                }}
-                            >
-                                Bạn quên mật khẩu?
-                            </Text>
-                        </Link>
-                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
-                            <TouchableOpacity
-                                style={{
-                                    shadowColor: 'rgba(0,0,0, .4)', // IOS
-                                    shadowOffset: { height: 1, width: 1 }, // IOS
-                                    shadowOpacity: 1, // IOS
-                                    shadowRadius: 1, //IOS
-                                    backgroundColor: '#fff',
-                                    elevation: 2, // Android
-                                    backgroundColor: 'rgb(250, 139, 158)',
-                                    borderRadius: 12,
-                                    padding: '3%',
-                                    marginRight: 20,
-                                    width: 120,
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}
-                                onPress={() => setFlag(false)}
-                            >
-                                <Text style={{ color: '#fff', fontSize: 17, fontWeight: 'bold' }}>Trở về</Text>
-                            </TouchableOpacity>
-                            <Link
-                                to='/home'
-                                style={{
-                                    shadowColor: 'rgba(0,0,0, .4)', // IOS
-                                    shadowOffset: { height: 1, width: 1 }, // IOS
-                                    shadowOpacity: 1, // IOS
-                                    shadowRadius: 1, //IOS
-                                    backgroundColor: '#fff',
-                                    elevation: 2, // Android
-                                    backgroundColor: 'rgb(250, 139, 158)',
-                                    borderRadius: 12,
-                                    padding: '3%',
-                                    width: 120,
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}
-                                // onPress={() => }
-                            >
-                                <Text style={{ color: '#fff', fontSize: 17, fontWeight: 'bold' }}>Đăng nhập</Text>
-                            </Link>
-                        </View>
-                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                            <Text>Bạn chưa có tài khoản?</Text>
-                            <Link to="/Register">
+                            <Text style={styles.errorMess}>Lỗi sdt</Text>
+                            <View style={{ flexDirection: 'row' }}>
+                                <TextInput style={styles.styleInput} placeholder="Mật khẩu" />
+                                <TouchableOpacity
+                                    style={{
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        alignSelf: 'center',
+                                        borderColor: 'cyan',
+                                        borderWidth: 1,
+                                        borderStyle: 'solid',
+                                        height: 40,
+                                        paddingLeft: 5,
+                                        paddingRight: 5,
+                                        marginLeft: 10,
+                                    }}
+                                >
+                                    <Icon name="eye-sharp" size={30} color="rgb(250, 139, 158)" />
+                                </TouchableOpacity>
+                            </View>
+                            <Text style={styles.errorMess}>Lỗi mk</Text>
+                            <Link to="/forgotPass" style={{ alignSelf: 'flex-end', marginRight: 22 }}>
                                 <Text
                                     style={{
                                         color: 'rgb(250, 139, 158)',
@@ -175,13 +124,70 @@ function Login() {
                                         textDecorationLine: 'underline',
                                     }}
                                 >
-                                    Đăng ký ngay
+                                    Bạn quên mật khẩu?
                                 </Text>
                             </Link>
+                            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                                <TouchableOpacity
+                                    style={{
+                                        shadowColor: 'rgba(0,0,0, .4)', // IOS
+                                        shadowOffset: { height: 1, width: 1 }, // IOS
+                                        shadowOpacity: 1, // IOS
+                                        shadowRadius: 1, //IOS
+                                        backgroundColor: '#fff',
+                                        elevation: 2, // Android
+                                        backgroundColor: 'rgb(250, 139, 158)',
+                                        borderRadius: 12,
+                                        padding: '3%',
+                                        marginRight: 20,
+                                        width: 120,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    }}
+                                    onPress={() => setFlag(false)}
+                                >
+                                    <Text style={{ color: '#fff', fontSize: 17, fontWeight: 'bold' }}>Trở về</Text>
+                                </TouchableOpacity>
+                                <Link
+                                    to="/home"
+                                    style={{
+                                        shadowColor: 'rgba(0,0,0, .4)', // IOS
+                                        shadowOffset: { height: 1, width: 1 }, // IOS
+                                        shadowOpacity: 1, // IOS
+                                        shadowRadius: 1, //IOS
+                                        backgroundColor: '#fff',
+                                        elevation: 2, // Android
+                                        backgroundColor: 'rgb(250, 139, 158)',
+                                        borderRadius: 12,
+                                        padding: '3%',
+                                        width: 120,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    }}
+                                    // onPress={() => }
+                                >
+                                    <Text style={{ color: '#fff', fontSize: 17, fontWeight: 'bold' }}>Đăng nhập</Text>
+                                </Link>
+                            </View>
+                            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                <Text>Bạn chưa có tài khoản?</Text>
+                                <Link to="/Register">
+                                    <Text
+                                        style={{
+                                            color: 'rgb(250, 139, 158)',
+                                            fontWeight: 'bold',
+                                            fontSize: 15,
+                                            textDecorationLine: 'underline',
+                                        }}
+                                    >
+                                        Đăng ký ngay
+                                    </Text>
+                                </Link>
+                            </View>
                         </View>
-                    </View>
-                </ImageBackground>
-            </View>
+                    </ImageBackground>
+                </View>
+            </Animatable.View>
         </SafeAreaView>
     );
 }
