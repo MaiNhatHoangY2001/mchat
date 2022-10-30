0d69893a6c8eb02d9157a15daef92faf34157019
mailong395 <maingoclong395@gmail.com>
2022-10-30 17:20:05 +0700

    fix search

--- client/src/components/Layout/LeftBar/ListFriend/ListFriend.js
+++ client/src/components/Layout/LeftBar/ListFriend/ListFriend.js
@@ -190,6 +190,7 @@ export default function ListFriend() {
                                     },
                                 }}
                                 size="small"
+                                fullWidth
                             />
                         )}
                     />
