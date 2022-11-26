eac43f4bfb14da195b213b31956ec893dc814d31
mailong395 <maingoclong395@gmail.com>
2022-11-27 06:02:09 +0700

    add change password

---
+++ client/src/components/Layout/LeftBar/Navbar/InpuText/InputTextCustom.js
@@ -0,0 +1,29 @@
+import classNames from 'classnames/bind';
+import styles from './Styles.scss';
+import { useState } from 'react';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+const cx = classNames.bind(styles);
+
+export default function TextInputCustom({ placeholder, onChangeText }) {
+    const [showPassword, setShowPassword] = useState(true);
+    const [typeInput, setTypeInput] = useState('password');
+
+    const handleShowPW = () => {
+        setShowPassword(!showPassword);
+        showPassword ? setTypeInput('text') : setTypeInput('password');
+    };
+
+    return (
+        <div className={cx('contain')}>
+            <input
+                onChange={(e) => onChangeText(e.target.value)}
+                className={cx('text-input')}
+                type={typeInput}
+                placeholder={placeholder}
+            />
+            <div onClick={handleShowPW}>{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}</div>
+        </div>
+    );
+}
