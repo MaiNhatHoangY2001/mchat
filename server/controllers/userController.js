7be90d43a38cd59fcb4fffee56f18a18c9e52c58
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2022-11-26 17:28:10 +0700

    add update profile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -45,6 +45,15 @@ const userController = {
 			res.status(500).json(error);
 		}
 	},
+	updateUser: async (req, res) => {
+		try {
+			const user = await User.findById(req.params.id);
+			await user.updateOne({ $set: req.body });
+			res.status(200).json('update successfully');
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
 
 	changePasswordWithPhoneNumber: async (req, res) => {
 		try {
